c71243dbba65ac07f50d20f15178de4ac91786c1
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-03-06 10:46:14 -0600

    ejercicio2.c

--- ejercicios/ejercicio2.c
+++ ejercicios/ejercicio2.c
@@ -4,33 +4,34 @@
 int main() {
     int m, n;
     printf("Ingrese el número de filas: ");
+    scanf("%d", &m);
     
     printf("Ingrese el número de columnas: ");
-    
+    scanf("%d", &n);
 
     int *matrix = (int *)calloc(m * n, sizeof(int));
     if (matrix == NULL) {
         printf("Error: No se pudo asignar memoria.\n");
-        
+        return 1;
     }
 
     printf("Matriz inicializada con ceros:\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            
+            printf("%d ", matrix[i * n + j]);
         }
         printf("\n");
     }
 
-    printf("Ingrese las coordenadas (fila, columna) y el valor para modificar (ej. 1 2 5):\n");
+    printf("Ingrese las coordenadas (fila, columna) y el valor para modificar\n");
     int row, col, value;
-    //lee los varlores solicitados
-    matrix[(row-1) * n + (col-1)] = value;
+    scanf("%d %d %d", &row, &col, &value);
+    matrix[(row - 1) * n + (col - 1)] = value;
 
     printf("Matriz actualizada:\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            
+            printf("%d ", matrix[i * n + j]);
         }
         printf("\n");
     }
