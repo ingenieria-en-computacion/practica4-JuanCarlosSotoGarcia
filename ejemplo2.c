f55cf6514c2e3b64866c2016493532823a6bd186
JuanCarlosSotoGarcia <carloscc1125@gmail.com>
2025-03-06 10:10:48 -0600

    ejemplo2.c

--- ejemplo2.c
+++ ejemplo2.c
@@ -5,6 +5,8 @@ int main(){
     int *arreglo, num, cont;
     printf("¿Cuántos elementos tiene el conjunto?\n");
     scanf("%d", &num);
+    printf("%p\n",arreglo);
+
     arreglo = (int*)calloc(num , sizeof(int));
     if(arreglo != NULL){
         printf
@@ -14,7 +16,8 @@ int main(){
         }
         printf("\t]\n");
         printf("Liberando el espacio reservado\n");
-        free(arreglo);        
+        free(arreglo);    
+        arreglo=NULL;    
     }
     return 0;
 }
